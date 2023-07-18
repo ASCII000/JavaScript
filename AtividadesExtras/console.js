3abc60c1188b1381c393f5c6b4d3e90b2252ebbc
r4n_emerson <r4n.oficial@gmail.com>
2023-07-18 20:17:14 -0300

    new

--- AtividadesExtras/console.js
+++ AtividadesExtras/console.js
@@ -4,8 +4,13 @@ let tamanho = text.length
 let hora = new Date()
 let horaAtual = hora
 
-let frutas = 'Maça, Laranja, Uva, Pera'
+let frutas = 'Maça, Laranja, Uva, Pera'.split(',')
+
+for (index in frutas) {
+    console.log(frutas[index])
+}
 
 // Métodos Strings.
-console.log(text.slice(0, 3))
-console.log(frutas.split(','))
+// console.log(text.slice(0, 3))
+// console.log(frutas.split(','))
+//  console.log(frutas.replace('Maça', 'Perâ'))
---
+++ aula08/Arrays.js
@@ -0,0 +1,4 @@
+let num = ['12', 12, true, 'Emerson']
+num.push(7) // Adicionar um numero
+console.log(num)
+console.log(num.indexOf(true))
