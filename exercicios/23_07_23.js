eb3f4bcb096681cb8a05abe1b41b957291a6c927
r4n_emerson <r4n.oficial@gmail.com>
2023-07-24 10:51:47 -0300

    Update 23_07_23.js

--- exercicios/23_07_23.js
+++ exercicios/23_07_23.js
@@ -28,3 +28,49 @@ function contarVogais(palavra) {
 console.log(contarVogais("Hello World"));
 console.log(contarVogais("JavaScript"));
 */
+
+/* #3
+function somaDigitos(numero) {
+    if (typeof numero != 'number' || !Number.isInteger(numero)) {
+        throw new Error("A função aceita somente numeros inteiros.");
+    }
+
+    const digitos = String(numero);
+    const soma = digitos.split('').reduce((acc, digito) => acc + parseInt(digito), 0);
+    return soma;
+}
+
+try {
+    console.log(somaDigitos(22))
+} catch (error) {
+    console.error(error.message)
+}
+*/
+
+/* # 4
+function inverterString(texto) {
+    if (typeof texto != 'string') {
+        throw new Error("A função só aceita textos.")
+    }
+    return texto.split('').reverse().join('')
+}
+
+console.log(inverterString("JavaScript"))
+*/
+/* #5
+function palindromo(palavra) {
+    if (typeof palavra !== "string") {
+      throw new Error("O tipo de dado tem que ser uma string!");
+    }
+  
+    const palavraSemEspacos = palavra.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return palavraSemEspacos === palavraSemEspacos.split("").reverse().join("");
+  }
+  
+  try {
+    console.log(palindromo("radar")); // Saída: true
+    console.log(palindromo("JavaScript")); // Saída: false
+  } catch (error) {
+    console.error(error.message);
+  }
+*/
